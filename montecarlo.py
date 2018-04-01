c3584532ead59b0928ba2dbc864f968aaed64632
Zachary Stevens <zed2808@gmail.com>
2018-04-01 13:32:51 -0400

    Fleshed out simulation and added move selection

--- montecarlo.py
+++ montecarlo.py
@@ -6,7 +6,7 @@ class MonteCarlo:
     # Takes a Board instance and initializes list of game states and stats
     def __init__(self, board, **kwargs):
         self.board = board
-        self.states = []
+        self.history = []
         self.wins = {}
         self.plays = {}
 
@@ -19,28 +19,76 @@ class MonteCarlo:
 
     # Takes game state and appends it to history
     def update(self, state):
-        self.states.append(state)
+        self.history.append(state)
 
     # Calculate the best move from the current state
     def get_play(self):
+        self.max_depth = 0
+        state = self.history[-1]
+        player = self.board.current_player(state)
+        legal_plays = self.board.legal_plays(self.history)
+
+        # Return early if 0 or 1 choices
+        if not legal_plays:
+            return
+        if len(legal) == 1:
+            return legal[0]
+
+        games = 0
         start_time = datetime.datetime.utcnow()
 
-        # Simulate until sim_time has elapsed
+        # Simulate only until sim_time has elapsed
         while datetime.datetime.utcnow() - start_time < self.sim_time:
             self.simulate()
+            games += 1
+
+        # Create a list of (play, state) tuples for each legal move and the state it will produce
+        plays_states = [(p, self.board.next_state(state, p)) for p in legal]
+
+        # Display the number of simulate() calls and the time elapsed
+        print(f'{games} games played in {datetime.datetime.utcnow() - start_time} seconds')
+
+        # Pick the move with the highest winrate
+        winrate, move = max((self.wins.get((player, s), 0) / self.plays.get((player, s), 1), p) for p, s in plays_states)
+
+        # Display the stats for each possible play
+
+        print(f'Maximum depth search: {self.max_depth}')
+
+        return move
 
     # Execute random playout from current state and update stats tables
     def simulate(self):
-        states_copy = self.states[:]
-        state = states_copy[-1]
+        visited_states = set()
+        history_copy = self.history[:]
+        state = history_copy[-1]
+        player = self.board.current_player(state)
 
-        for i in range(self.max_moves):
-            legal_plays = self.board.legal_plays(states_copy)
+        expand = True
+        for t in range(self.max_moves):
+            legal_plays = self.board.legal_plays(history_copy)
 
             play = random.choice(legal_plays)
             state = self.board.next_state(state, play)
-            states_copy.append(state)
+            history_copy.append(state)
 
-            winner = self.board.winner(states_copy)
+            # Player refers to the player that moved into the state
+            if expand and (player, state) not in self.plays:
+                expand = False
+                self.plays[(player, state)] = 0
+                self.wins[(player, state)] = 0
+
+            visited_states.add((player, state))
+
+            player = self.board.current_player(state)
+            winner = self.board.winner(history_copy)
             if winner:
                 break
+
+        for player, state in visited_states:
+            if (player, state) not in self.plays:
+                continue
+            self.plays[(player, state)] += 1
+            if player == winner:
+                self.wins[(player, state)] += 1
+
