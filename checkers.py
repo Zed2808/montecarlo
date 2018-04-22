5b4ac87ce9221a0bcd1b6876781f5e37e04ea1c2
Zachary Stevens <zed2808@gmail.com>
2018-04-22 15:01:43 -0400

    Check for and force jumps (still need to add double-jumps)

--- checkers.py
+++ checkers.py
@@ -1,3 +1,8 @@
+black_pieces = ['○', '◔']
+white_pieces = ['●', '◕']
+black_king = '◔'
+white_king = '◕'
+
 # Returns starting state of a new game
 def new_game():
     return {'board': [['', '●', '', '●', '', '●', '', '●'],
@@ -24,6 +29,13 @@ def in_bounds(coords):
 # [(41, 34), (43, 34), (43, 36), ...] Space number
 # [((5, 0), (4, 1)), ((5, 2), (4, 1)), ((5, 2), (4, 3)), ...]
 def legal_plays(state):
+    # First check for any legal jumps
+    jumps = legal_jumps(state)
+
+    # If there are jumps available, they must be taken
+    if jumps:
+        return jumps
+
     board = state['board']
     player = state['player']
 
@@ -34,7 +46,7 @@ def legal_plays(state):
         # Iterate over each space in the row
         for colnum, col in enumerate(row):
             # Space occupied by black
-            if (col == '○' or col == '◔') and player == 0:
+            if col in black_pieces and player == 0:
                 # Check up/left & up/right diagonals for empty spaces
                 if in_bounds((rownum-1, colnum-1)) and board[rownum-1][colnum-1] == '':
                     legal.append(((rownum, colnum), (rownum-1, colnum-1)))
@@ -43,7 +55,7 @@ def legal_plays(state):
                     legal.append(((rownum, colnum), (rownum-1, colnum+1)))
 
             # Check extra spaces for black kings
-            if col == '◔' and player == 0:
+            if col == black_king and player == 0:
                 # Check down/left & down/right diagonals for empty spaces
                 if in_bounds((rownum+1, colnum-1)) and board[rownum+1][colnum-1] == '':
                     legal.append(((rownum, colnum), (rownum+1, colnum-1)))
@@ -52,7 +64,7 @@ def legal_plays(state):
                     legal.append(((rownum, colnum), (rownum+1, colnum+1)))
 
             # Space is occupied by white
-            if (col == '●' or col == '◕') and player == 1:
+            if col in white_pieces and player == 1:
                 # Check down/left & down/right diagonals for empty spaces
                 if in_bounds((rownum+1, colnum-1)) and board[rownum+1][colnum-1] == '':
                     legal.append(((rownum, colnum), (rownum+1, colnum-1)))
@@ -61,7 +73,7 @@ def legal_plays(state):
                     legal.append(((rownum, colnum), (rownum+1, colnum+1)))
 
             # Check extra spaces for white kings
-            if col == '◕' and player == 1:
+            if col == white_king and player == 1:
                 # Check up/left & up/right diagonals for empty spaces
                 if in_bounds((rownum-1, colnum-1)) and board[rownum-1][colnum-1] == '':
                     legal.append(((rownum, colnum), (rownum-1, colnum-1)))
@@ -69,7 +81,74 @@ def legal_plays(state):
                 if in_bounds((rownum-1, colnum+1)) and board[rownum-1][colnum+1] == '':
                     legal.append(((rownum, colnum), (rownum-1, colnum+1)))
 
-    print(f'Legal moves found: {legal}')
+    return legal
+
+# Returns a list of legal jumps available to the current player
+def legal_jumps(state):
+    board = state['board']
+    player = state['player']
+
+    legal = []
+
+    # Iterate over each space on the board
+    for rownum, row in enumerate(board):
+        for colnum, col in enumerate(row):
+            # Check forward jumps available to black
+            if col in black_pieces and player == 0:
+                # Space two up/two left is in bounds and empty
+                if in_bounds((rownum-2, colnum-2)) and board[rownum-2][colnum-2] == '':
+                    # Space directly up/left is a white piece
+                    if board[rownum-1][colnum-1] in white_pieces:
+                        legal.append( ((rownum, colnum), (rownum-2, colnum-2)) )
+
+                # Space 2up/2right is in bounds and empty
+                if in_bounds((rownum-2, colnum+2)) and board[rownum-2][colnum+2] == '':
+                    # Space directly up/right is a white piece
+                    if board[rownum-1][colnum+1] in white_pieces:
+                        legal.append( ((rownum, colnum), (rownum-2, colnum+2)) )
+
+            # Check backward jumps available to black
+            if col == black_king and player == 0:
+                # Space 2down/2left is in bounds and empty
+                if in_bounds((rownum+2, colnum-2)) and board[rownum+2][colnum-2] == '':
+                    # Space directly down/left is a white piece
+                    if board[rownum+1][colnum-1] in white_pieces:
+                        legal.append( ((rownum, colnum), (rownum+2, colnum-2)) )
+
+                # Space 2down/2right is in bounds and empty
+                if in_bounds((rownum+2, colnum+2)) and board[rownum+2][colnum+2] == '':
+                    # Space directly down/right is a white piece
+                    if board[rownum+1][colnum+1] in white_pieces:
+                        legal.append( ((rownum, colnum), (rownum+2, colnum+2)) )
+
+            # Check forward jumps available to white
+            if col in white_pieces and player == 1:
+                # Space 2down/2left is in bounds and empty
+                if in_bounds((rownum+2, colnum-2)) and board[rownum+2][colnum-2] == '':
+                    # Space directly down/left is a black piece
+                    if board[rownum+1][colnum-1] in black_pieces:
+                        legal.append( ((rownum, colnum), (rownum+2, colnum-2)) )
+
+                # Space 2down/2right is in bounds and empty
+                if in_bounds((rownum+2, colnum+2)) and board[rownum+2][colnum+2] == '':
+                    # Space directly down/right is a black piece
+                    if board[rownum+1][colnum+1] in black_pieces:
+                        legal.append( ((rownum, colnum), (rownum+2, colnum+2)) )
+
+            # Check backward jumps available to white
+            if col == white_king and player == 1:
+                # Space 2up/2left is in bounds and empty
+                if in_bounds((rownum-2, colnum-2)) and board[rownum-2][colnum-2] == '':
+                    # Space directly up/left is a black piece
+                    if board[rownum-1][colnum-1] in black_pieces:
+                        legal.append( ((rownum, colnum), (rownum-2, colnum-2)) )
+
+                # Space 2up/2right is in bounds and empty
+                if in_bounds((rownum-2, colnum+2)) and board[rownum-2][colnum+2] == '':
+                    # Space directly up/right is a black piece
+                    if board[rownum-1][colnum+1] in black_pieces:
+                        legal.append( ((rownum, colnum), (rownum-2, colnum+2)) )
+
     return legal
 
 # Alters game state according to the play made
@@ -81,14 +160,20 @@ def next_state(state, play):
 
     # Determine whether piece should become a king
     if dest_row == 0 or dest_row == 7:
-        if piece == '○':
-            piece = '◔'
-        elif piece == '●':
-            piece = '◕'
+        if piece in black_pieces:
+            piece = black_king
+        elif piece in white_pieces:
+            piece = white_king
 
     # Move piece from origin to destination
     state['board'][origin_row][origin_col] = ''
     state['board'][dest_row][dest_col] = piece
+
+    # If a piece was jumped, remove it from the game
+    if abs(origin_row - dest_row) == 2:
+        jumped_row = (origin_row + dest_row) // 2
+        jumped_col = (origin_col + dest_col) // 2
+        state['board'][jumped_row][jumped_col] = ''
 
     # Change active player
     state['player'] = 1 if state['player'] == 0 else 0
@@ -101,9 +186,9 @@ def winner(state):
     # Iterate through all spaces, count number of each piece
     for row in board:
         for col in row:
-            if col == '○':
+            if col in black_pieces:
                 num_black += 1
-            elif col == '●':
+            elif col in white_pieces:
                 num_white += 1
 
     # If there are 0 of one color, the other color wins
--- play_checkers.py
+++ play_checkers.py
@@ -6,8 +6,15 @@ state = checkers.new_game()
 print(checkers.to_string(state))
 
 while True:
+    input()
+
     legal = checkers.legal_plays(state)
-    play = random.choice(legal)
+
+    try:
+        play = random.choice(legal)
+    except:
+        print('No more moves available.')
+        break
 
     checkers.next_state(state, play)
 
